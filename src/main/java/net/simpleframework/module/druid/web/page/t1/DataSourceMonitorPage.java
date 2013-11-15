76c79d75e0ba7f3ef6745b53eca47fc2f80dbda6
simpleframework <cknet@126.com>
2013-11-15 11:41:06 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/module/druid/web/page/t1/DataSourceMonitorPage.java
+++ src/main/java/net/simpleframework/module/druid/web/page/t1/DataSourceMonitorPage.java
@@ -36,7 +36,7 @@ import com.alibaba.druid.stat.JdbcStatManager;
  * Licensed under the Apache License, Version 2.0
  * 
  * @author 陈侃(cknet@126.com, 13910090885)
- *         http://code.google.com/p/simpleframework/
+ *         https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 @PageMapping(url = "/druid/datasource")
