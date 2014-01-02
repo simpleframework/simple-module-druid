881e98e6f49e18be7afe709190baaffcb0491786
simpleframework <cknet@126.com>
2014-01-02 16:03:20 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/module/druid/web/DruidWebContext.java
+++ src/main/java/net/simpleframework/module/druid/web/DruidWebContext.java
@@ -3,25 +3,36 @@ package net.simpleframework.module.druid.web;
 import static net.simpleframework.common.I18n.$m;
 import net.simpleframework.ctx.AbstractModuleContext;
 import net.simpleframework.ctx.Module;
+import net.simpleframework.ctx.ModuleFunctions;
 import net.simpleframework.ctx.permission.IPermissionConst;
 import net.simpleframework.module.druid.web.page.t1.DataSourceMonitorPage;
 import net.simpleframework.mvc.ctx.WebModuleFunction;
 
+/**
+ * Licensed under the Apache License, Version 2.0
+ * 
+ * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
+ *         http://www.simpleframework.net
+ */
 public class DruidWebContext extends AbstractModuleContext implements IDruidContext {
 
 	@Override
 	protected Module createModule() {
-		return new Module()
-				.setName("simple-module-druid")
-				.setText("Druid")
-				.setDefaultFunction(
-						new WebModuleFunction(DataSourceMonitorPage.class).setName(
-								"simple-module-druid-DataSourceMonitorPage").setText(
-								$m("DruidWebContext.0"))).setOrder(36);
+		return new Module().setName(MODULE_NAME).setText("Druid").setOrder(36)
+				.setDefaultFunction(FUNC_DATASOURCE_MONITOR);
+	}
+
+	@Override
+	protected ModuleFunctions getFunctions() {
+		return ModuleFunctions.of(FUNC_DATASOURCE_MONITOR);
 	}
 
 	@Override
 	public String getManagerRole() {
 		return IPermissionConst.ROLE_MANAGER;
 	}
+
+	public static final WebModuleFunction FUNC_DATASOURCE_MONITOR = (WebModuleFunction) new WebModuleFunction(
+			DataSourceMonitorPage.class).setName(MODULE_NAME + "-DataSourceMonitorPage").setText(
+			$m("DruidWebContext.0"));
 }
--- src/main/java/net/simpleframework/module/druid/web/IDruidContext.java
+++ src/main/java/net/simpleframework/module/druid/web/IDruidContext.java
@@ -2,5 +2,13 @@ package net.simpleframework.module.druid.web;
 
 import net.simpleframework.ctx.IModuleContext;
 
+/**
+ * Licensed under the Apache License, Version 2.0
+ * 
+ * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
+ *         http://www.simpleframework.net
+ */
 public interface IDruidContext extends IModuleContext {
+
+	static String MODULE_NAME = "simple-module-druid";
 }
