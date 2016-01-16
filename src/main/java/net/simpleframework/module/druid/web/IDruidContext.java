91ff4455b50029db3242ac1df6456bec6cb83015
CHENK <CHENK@CHENK-PC>
2016-01-16 16:11:07 +0800

    Signed-off-by: CHENK <CHENK@CHENK-PC>

--- src/main/java/net/simpleframework/module/druid/web/IDruidContext.java
+++ src/main/java/net/simpleframework/module/druid/web/IDruidContext.java
@@ -1,7 +1,6 @@
 package net.simpleframework.module.druid.web;
 
 import net.simpleframework.ctx.IModuleContext;
-import net.simpleframework.mvc.IMVCContextVar;
 
 /**
  * Licensed under the Apache License, Version 2.0
@@ -9,7 +8,7 @@ import net.simpleframework.mvc.IMVCContextVar;
  * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
-public interface IDruidContext extends IModuleContext, IMVCContextVar {
+public interface IDruidContext extends IModuleContext {
 
 	static String MODULE_NAME = "simple-module-druid";
 }
